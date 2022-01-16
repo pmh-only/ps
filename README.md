791680afecdad545d821be3decfc7d2bcfe0517d
Park Min Hyeok <pmh_only@pmh.codes>
2022-01-16 23:53:15 +0900

    feat: add disable-windows-defender script

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ powershell scripts
 ```ps1
 iwr pmh.codes/ps/list | iex
 ```
+
+## script list
+- `iwr pmh.codes/ps/list | iex` - list scripts
+- `iwr pmh.codes/ps/admin | iex` - run Powershell as Administrator
+- `iwr pmh.codes/ps/rick | iex` - ???
+- `iwr pmh.codes/ps/uac | iex` - disable UAC prompt
+- `iwr pmh.codes/ps/def | iex` - disable Windows Defender
+
+## powered by
+- github pages
+- cloudflare transform rule
--- def.ps1
+++ def.ps1
@@ -1,6 +1,7 @@
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
   Write-Output "not elevated, try 'iwr pmh.codes/ps/admin | iex'"
 } else {
-  Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
-  Write-Output "Windows UAC has been Disabled"
+  Set-MpPreference -DisableRealtimeMonitoring $true
+  New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
+  Write-Output "Windows Defender has been Disabled"
 }
